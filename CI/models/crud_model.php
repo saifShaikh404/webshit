309d4acb965965643319897929e423ac3a5c155d
saifShaikh404 <132943658+saifShaikh404@users.noreply.github.com>
2023-07-27 20:40:56 +0530

    Add files via upload

--- CI/models/crud_model.php
+++ CI/models/crud_model.php
@@ -23,7 +23,7 @@ class Crud_model extends CI_model{
         foreach ($keyVal as $value) {
             // All preposition , all helping verb
             // $ignoredWords = array("and", "the", "is", "are", "was", "were", "in", "on", "at", "with", "for", "to", "from");
-            if($value == "am" || $value == "is" || $value == "are" || $value == "on" || $value == "the" || $value == "in" || $value == "was"){
+            if($value == "am" || $value == "from" || $value == "with" || $value == "to" || $value == "for" || $value == "is" || $value == "are" || $value == "and" || $value == "at" || $value == "on" || $value == "the" || $value == "in" || $value == "was"){
                 continue;
             }
             else{
@@ -43,37 +43,102 @@ class Crud_model extends CI_model{
 
     }
 
-    // public function search($keyVal){
+    public function getSearch($keyVal){
+        $check = "";
+        $this->db->select('*');
+        $this->db->from('test');
+        $this->db->group_start();
+        foreach ($keyVal as $value) {
+            if($value == "am" || $value == "from" || $value == "with" || $value == "to" || $value == "for" || $value == "is" || $value == "are" || $value == "and" || $value == "at" || $value == "on" || $value == "the" || $value == "in" || $value == "was"){
+                continue;
+            }
+            else{
+                $this->db->or_like('LOWER(name)', $value);
+                $check = $check . $value;
+            }
+        }
+        $this->db->group_end();
+
+        if(empty($check)){
+            $query1 = "";
+        }
+        else{
+            $query1 = $this->db->get();
+        }
+
+        // table 2
+        $check2 = "";
+        $this->db->select('*');
+        $this->db->from('test2');
+        $this->db->group_start();
+        foreach ($keyVal as $value) {
+            if($value == "am" || $value == "from" || $value == "with" || $value == "to" || $value == "for" || $value == "is" || $value == "are" || $value == "and" || $value == "at" || $value == "on" || $value == "the" || $value == "in" || $value == "was"){
+                continue;
+            }
+            else{
+                $this->db->or_like('LOWER(name)', $value);
+                $check2 = $check2 . $value;
+            }
+        }
+        $this->db->group_end();
+
+        if(empty($check2)){
+            $query2 = "";
+        }
+        else{
+            $query2 = $this->db->get();
+        }
         
-    //     $this->db->select('*');
-    //     $this->db->from('datas');
-    //     $this->db->group_start();
-    //     foreach ($keyVal as $value) {
-    //         if($value == "am" || $value == "is" || $value == "are" || $value == "on" || $value == "the" || $value == "in" || $value == "was"){
-    //             continue;
-    //         }
-    //         else{
-    //             $this->db->or_like('LOWER(name)', $value);
-    //         }
-    //     }
-    //     $this->db->group_end();
-    //     $query = $this->db->get();
-    //     // print_r($this->db->get());
+        // table 3
+        $check3 = "";
+        $this->db->select('*');
+        $this->db->from('test3');
+        $this->db->group_start();
+        foreach ($keyVal as $value) {
+            if($value == "am" || $value == "from" || $value == "with" || $value == "to" || $value == "for" || $value == "is" || $value == "are" || $value == "and" || $value == "at" || $value == "on" || $value == "the" || $value == "in" || $value == "was"){
+                continue;
+            }
+            else{
+                $this->db->or_like('LOWER(name)', $value);
+                $check3 = $check3 . $value;
+            }
+        }
+        $this->db->group_end();
 
-    //     if($this->db->get()){
-    //         // $query = $this->db->get();
-    //         return $this->db->get()->result_array(); 
-    //     }
-    //     else{
-    //         print_r("Err");
-    //         return "";
-             
-    //     }
+        if(empty($check3)){
+            $query3 = "";
+        }
+        else{
+            $query3 = $this->db->get();
+        }
 
-    // }
-
-    public function getEnumVal($enumVal){
-
+        if($query1 == "" && $query2 == "" && $query3 == ""){
+            return "";
+        }else if($query1 == "" && $query2 != "" && $query3 != ""){
+            $resultArray = array_merge($query2->result_array(), $query3->result_array());
+            return $resultArray;
+        }
+        else if($query1 != "" && $query2 == "" && $query3 != ""){
+            $resultArray = array_merge($query1->result_array(), $query3->result_array());
+            return $resultArray;
+        }
+        else if($query1 != "" && $query2 != "" && $query3 == ""){
+            $resultArray = array_merge($query1->result_array(), $query2->result_array());
+            return $resultArray;
+        }
+        else if($query1 == "" && $query2 == "" && $query3 != ""){
+            return $query3->result_array();
+        }
+        else if($query1 == "" && $query2 != "" && $query3 == ""){
+            return $query2->result_array();
+        }
+        else if($query1 != "" && $query2 == "" && $query3 == ""){
+            return $query1->result_array();
+        }
+        else{
+            $resultArray = array_merge($query1->result_array(), $query2->result_array(), $query3->result_array());
+            return $resultArray;
+        }
     }
 
 }
